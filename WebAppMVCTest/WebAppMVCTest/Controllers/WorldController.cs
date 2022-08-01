5a1066fae14878939f674d8f8462670894e68138
DESKTOP-UCN82LQ\Илья <ilya.karnilov2@gmail.com>
2022-08-01 14:31:16 +0300

    Add new controller

---
+++ WebAppMVCTest/WebAppMVCTest/Controllers/WorldController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMVCTest.Controllers;
+
+public class WorldController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
